a9f70958a2567abee56ae73963b4f6af27793f25
Nevzat Mataraci <91319943+ntmataraci@users.noreply.github.com>
2022-03-03 20:21:52 +0300

    Add files via upload

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,7 @@
 // pages/404.js
-export default function Custom404() {
-    return <div style={{marginTop:"20%"}}><h3>I'm sorry to say this clone only for Home Section</h3></div>
+
+const  Custom404 = ()=> {
+    return (<div style={{marginTop:"20%"}}><h3>I m sorry to say this clone only for Home Section</h3></div>)
   }
+
+  export default Custom404
