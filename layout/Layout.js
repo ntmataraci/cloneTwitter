2053e18601396b4fd5a078b25e914f0d083f906a
Nevzat Mataraci <91319943+ntmataraci@users.noreply.github.com>
2022-03-04 12:41:12 +0300

    Add files via upload

--- layout/Layout.js
+++ layout/Layout.js
@@ -3,6 +3,11 @@ import RightSideBar from "./RightSideBar"
 import Messages from "../UI/Messages";
 import Main from "./Main"
 import { useState,useEffect } from "react";
+import Header from "../mobile/Header";
+import MessageBlock from "../components/MessageBlock"
+import messages from "../datas/messages";
+import LeftOpenMenu from "../mobile/LeftOpenMenu";
+import Bottom from "../mobile/Bottom";
 const Layout = ({children}) =>{
 
     const [windowSize,setWindowSize]=useState({
@@ -26,12 +31,25 @@ const Layout = ({children}) =>{
         handleResize()
       },[])
     
-
-
-
+const [menuOp,setMenuOp]=useState(false)
+const menuSit = (x) =>{ 
+setMenuOp(x)
+}
+//width under is mobile
 return(
     
         <>
+        {windowSize.width<600?
+        <>
+        <Header getMenuOp = {menuOp} menuHandler={menuSit}/>
+
+        <LeftOpenMenu menuWidth={menuOp} />
+
+        <MessageBlock myData={messages}/>
+        <Bottom/>
+        </>
+        :
+
 <div style={{display:"flex",flexWrap:"wrap"}}>
 <LeftSideBar getSize={windowSize}/>
 <Main>
@@ -40,7 +58,7 @@ return(
   {windowSize.width>1000&& <RightSideBar/>}
     <Messages/>
 </div>
-
+}
 
 </>
     )
